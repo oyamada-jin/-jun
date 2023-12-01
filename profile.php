6d63d69a056ebd028a14db0ffb7f3d01cb83f362
rizaiya <jin20030127@icloud.com>
2023-12-01 12:02:24 +0900

    変更

--- profile.php
+++ profile.php
@@ -7,6 +7,9 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>プロフィール</title>
+
+    <link rel="stylesheet" href="css/header.css?<?php echo date('YmdHis'); ?>">
+
 </head>
 <body>
      <!-- ヘッダーここから -->
